508965651e5360dd075a855df1528fc36562dfc6
vy_morozov <vy_morozov@betboom.org>
2022-04-08 18:30:32 +0300

    readline works

--- v3-method-creator/index.js
+++ v3-method-creator/index.js
@@ -1,13 +1,93 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let methodName = '';
-let path = '';
+const labels = [
+    {
+        question: '',
+        label: '',
+        answer: '',
+    },
+    'methodName', 'basePath'
+];
+const answersList = ['Название метода? (через пробел)', 'Путь до сервиса?'];
+// let methodName = '';
+// let basePath = '';
+
+// function getDiceAnswer() {
+//     return new Promise(resolve => {
+//         rl.question("Test ?: ", (answer) => {
+//             resolve(answer);
+//             console.log("test: ", answer);
+//             rl.close();
+//         });
+//     });
+// }
+
+const answers = [
+    {
+        question: '',
+
+    }
+];
+
+function ask(questionText, answer, label) {
+    return new Promise((resolve, reject) => {
+        rl.question(questionText, (answer) => {
+            answers.push(answer);
+            resolve(answer);
+            console.log("test: ", answer);
+        });
+    });
+}
+
+async function getAnswers() {
+    for (let label of labels) {
+        await ask(label.question);
+    }
+    rl.close();
+    //this line won't execute until the answer is ready
+    console.log(answers)
+}
+
+getAnswers();
+
+// function ask(questionText) {
+//     return new Promise((resolve, reject) => {
+//         rl.question(questionText, (input) => resolve(input));
+//     });
+// }
+
+// ask('Название метода? (через пробел)')
+//     .then((result) => {
+//         methodName = result;
+//         console.log(result);
+//         return ask('Путь до сервиса?');
+//     })
+//     .then(result => {
+//         basePath = result;
+//         console.log(result);
+//         rl.close();
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // спрашивать путь до папки с проектом
 // спрашивать нужно ли добавлять шаблон документации для метода
     // если нужна дока - спросить желаете ли ввести входные/выходные параметры
@@ -15,22 +95,14 @@ let path = '';
 // спрашивать нужно ли добавлять шаблон для автотестов
 // можно ещё спрашивать за коллекцию для постмана
 
-rl.question('Название метода? (вводить через пробел)', (answer) => {
-    console.log(`Принятое название: ${answer}`);
-    methodName = answer;
-    rl.close();
-});
-rl.question('Путь до папки с сервисом?', (answer) => {
-    console.log(`Принятый путь до папки: ${answer}`);
-    methodName = answer;
-    rl.close();
-});
-
 // преобразовать название в kebab-case
 // преобразовать название в PascalCase
 // преобразовать название в camelCase
 
 // создать папку с методом
+// fs.mkdir(path.join(basePath, 'src/lib/methods/test'), err => {
+//     console.log(err)
+// })
 // создать файл index.js
 // создать файл method-schema.js
 // добавить ссылки на метод в корневой для methods файл index.js
@@ -50,13 +122,6 @@ rl.question('Путь до папки с сервисом?', (answer) => {
 // TODO проверять на длину
 // TODO проверять на русские буквы
 // TODO предлагать подтверждение результата
-
-
-// Это работает:
-// const fs = require('fs');
-// const path = require('path');
-
-// const basePath = '/home/vladimir_m/bb/v3/erai';
 
 // fs.mkdir(path.join(basePath, 'src/lib/methods/test'), err => {
 //     console.log(err)
