9576d57ebdc72811ab003a46cba93b6c94e7b85a
Tropik42 <aviavova94@gmail.com>
2022-06-07 00:27:04 +0300

    express-like server started

--- node js ulbi tv/index.js
+++ node js ulbi tv/index.js
@@ -1,6 +1,27 @@
 const http = require('http');
-
+const EventEmitter = require('events');
 const PORT = process.env.PORT || 5000;
+
+const emitter = new EventEmitter();
+
+class Router {
+    constructor() {
+        this.endpoints = {}
+    }
+
+    request(method = 'GET', path, handler) {
+        if(!this.endpoints[path]) {
+            this.endpoints[path] = {}
+        }
+        const endpoint = this.endpoints[path];
+
+        if(endpoint[method]) {
+            throw new Error(`[${method}] по адресу ${path} уже существует`)
+        }
+
+        endpoint[method] = handler
+    }
+}
 
 const server = http.createServer((req, res) => {
     // res.writeHead(200, {
